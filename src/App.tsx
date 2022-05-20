32661252af705080b80f949be9e2a410843d271b
Darlon Henrique <darlonhenrisouza@gmail.com>
2022-05-20 18:18:46 -0300

    feat(components): create home components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,30 @@
+import { Dashboard } from './components/Dashboard'
+import Modal from 'react-modal'
+import { Header } from './components/Header'
+import { GlobalStyle } from './styles/global'
+import { useState } from 'react'
+
+Modal.setAppElement('#root')
+
 export function App() {
-  return <h1>Hello World</h1>
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTranactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+
+  return (
+    <>
+      <Header onOpenNewTransactionModal={handleOpenNewTranactionModal} />
+      <Dashboard />
+      <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}>
+        <h2>Testando modal</h2>
+      </Modal>
+      <GlobalStyle />
+    </>
+  )
 }
