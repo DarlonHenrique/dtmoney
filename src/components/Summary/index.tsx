bce0749b323f1c6e5d605cd9c3b05f51909e2079
Darlon Henrique <darlonhenrisouza@gmail.com>
2022-05-22 06:50:45 -0300

    feat(summary): add dinamically numbers to summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -8,6 +8,25 @@ import { TransactionsContext } from '../../TransactionsContext'
 export function Summary() {
   const { transactions } = useContext(TransactionsContext)
 
+  const summary = transactions.reduce(
+    (accumulator, transaction) => {
+      if (transaction.type === 'deposit') {
+        accumulator.deposit += transaction.amount
+        accumulator.total += transaction.amount
+      } else {
+        accumulator.withdraw += transaction.amount
+        accumulator.total -= transaction.amount
+      }
+
+      return accumulator
+    },
+    {
+      deposit: 0,
+      withdraw: 0,
+      total: 0
+    }
+  )
+
   return (
     <Container>
       <div>
@@ -15,7 +34,12 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeImg} alt='Entradas' />
         </header>
-        <strong>R$1.000,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.deposit)}
+        </strong>
       </div>
 
       <div>
@@ -23,7 +47,13 @@ export function Summary() {
           <p>Saídas</p>
           <img src={outcome} alt='Saídas' />
         </header>
-        <strong>-R$500,00</strong>
+        <strong>
+          -
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.withdraw)}
+        </strong>
       </div>
 
       <div className='highlight-background'>
@@ -31,7 +61,12 @@ export function Summary() {
           <p>Total</p>
           <img src={total} alt='Total' />
         </header>
-        <strong>R$500,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.total)}
+        </strong>
       </div>
     </Container>
   )
